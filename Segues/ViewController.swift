d397e13bded619d5fd4636cde450f14d45c6d926
Guest User <Guest@berlin.local>
2018-12-18 16:07:58 +0700

    Initial Commit

---
+++ Segues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Guest User on 12/18/18.
+//  Copyright © 2018 Guest User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
